cf5fa888d6698730d9e3efba59eef20160e7765d
Megan <codebymegan@gmail.com>
2024-11-03 16:54:26 +0100

    uploading local files

---
+++ PYTHON/Lektion1/FruitListManager.py
@@ -0,0 +1,6 @@
+#Create and manipulate a list: Write a program to create a list of your favorite fruits. 
+#Add at least two more fruits to the list using append. 
+# Then remove one fruit and replace it with a different one. 
+# Finally, print the modified list.
+#Expected Output: A list of fruits, showing additions and replacements.
+
